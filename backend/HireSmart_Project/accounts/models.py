ad4778e7238b4b5d22edd4b8902e4132080bfe29
Pandurang Bhosale <pandurangbhosale2004@gmail.com>
2026-07-22 22:43:02 +0530

    Implement custom user model

--- backend/HireSmart_Project/accounts/models.py
+++ backend/HireSmart_Project/accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        JOB_SEEKER = "JOB_SEEKER", "Job Seeker"
+        RECRUITER = "RECRUITER", "Recruiter"
+        ADMIN = "ADMIN", "Admin"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.JOB_SEEKER,
+    )
+
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
